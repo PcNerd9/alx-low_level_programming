ee95700b5de215701d9157f3fbec7b3140956f65
PcNerd <ajayihabeeb977@gmail.com>
2023-07-14 22:10:01 +0100

    Wrote a program that prints all possible combincations of two two_digits numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					if ((i < k) || (i == k && j < l))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+					if (!(i == 57 && j == 56))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
