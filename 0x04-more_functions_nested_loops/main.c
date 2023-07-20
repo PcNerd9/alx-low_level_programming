27439ceb2ce1a7363b7acddb0c8f935146a3ec14
PcNerd <ajayihabeeb977@gmail.com>
2023-07-20 12:21:15 +0100

    wrote a c function that prints a square of n size

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -8,11 +8,8 @@
  */
 int main(void)
 {
-	char c;
-
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
+	print_square(2);
+    print_square(10);
+    print_square(0);
 	return (0);
 }
