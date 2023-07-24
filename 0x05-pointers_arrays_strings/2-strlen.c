374482fa003c37aa943499ad04b22e87e187956b
PcNerd <ajayihabeeb977@gmail.com>
2023-07-24 12:13:56 +0100

    wrote a c function that prints a string using _putchar to stdout

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -16,5 +16,5 @@ int _strlen(char *s)
 	{
 		lenght += 1;
 	}
-	return lenght;
+	return (lenght);
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by new line to stdout
+ * @str: a pointer to the string to be printed
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
