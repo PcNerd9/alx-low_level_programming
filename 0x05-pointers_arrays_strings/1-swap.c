9dc8b63257dacc65508c806c2b4f2c046996ae34
PcNerd <ajayihabeeb977@gmail.com>
2023-07-24 12:07:21 +0100

    wrote a c function that returns the lenght of a string

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - the function swap the values in variables
+ * @a: the pointer to the first value
+ * @b: the pointer to the second value
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	
+	c = *b;
+	*b = *a;
+	*a = c;
+}
