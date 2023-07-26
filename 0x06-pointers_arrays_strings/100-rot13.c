3b0650a3e006aa495bc6a62029ded3b0a4c92732
PcNerd <ajayihabeeb977@gmail.com>
2023-07-26 18:37:17 +0100

    wrote a c function that encode a string into rot13 encryption format

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encode a string using rot13 encryption format 
+ *@str: the string to encode
+ *
+ *Return: encoded string
+ */
+
+char *rot13(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+		{
+			str[i] = str[i] + 13;
+		}
+		else if ((str[i] > 'm' && str[i] <= 'z') || (str[i] > 'M' && str[i] <= 'Z'))
+		{
+			str[i] = str[i] - 13;
+		}
+	}
+	return (str);
+}
