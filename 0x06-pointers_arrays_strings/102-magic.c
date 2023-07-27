db0f1ce727d3ff6c5ac0a5dd814ec7555594f18c
PcNerd <ajayihabeeb977@gmail.com>
2023-07-27 23:29:47 +0100

    modify my 102-magic.c file to betty standard

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,10 +16,10 @@ int main(void)
 	/*
 	 * write your line of code here...
 	 * Remember:
-	 * -you are not allowed to use a
-	 *  - you are not allowed to modify p
-	 *  - one one statement
-	 *  - you are not allowed to code anything else than this line of code
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - one one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this print 98\n */
