ebeb47181f5bebf042c7bc9d40a1c158de691d05
PcNerd <ajayihabeeb977@gmail.com>
2023-07-31 17:32:22 +0100

    wrote a a c function that locates a substring in a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+
+/**
+ * _strstr - locates a substring. it worl exactly like the strstr func
+ *@haystack: points the the string to search a substring from
+ *@needle: points to the string to search in another string
+ *
+ * Return: a pointer to the location of the first occurence else retrn 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int it_contain;
+
+	while (*haystack != '\0')
+	{
+		it_contain = 0;
+		if (*haystack == needle[0])
+		{
+			for (i = 0; needle[i] != '\0'; i++)
+			{
+				if (*(haystack + i) == needle[i])
+				{
+					it_contain = 1;
+				}
+				else
+				{
+					it_contain = 0;
+					break;
+				}
+			}
+			if (it_contain)
+			{
+				return (haystack);
+			}
+			else
+				haystack += i - 1;
+		}
+		haystack++;
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
@@ -36,19 +36,12 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-	    int c3[3][3] = {
-        {0, 1, 5},
-        {10, 11, 12},
-        {1000, 101, 102},
-    };
-    int c5[5][5] = {
-        {0, 1, 5, 12124, 1234},
-        {10, 11, 12, 123521, 12512},
-        {1000, 101, 102, 12545, 214543435},
-        {100, 1012451, 11102, 12545, 214543435},
-        {10, 12401, 10452, 11542545, 1214543435},
-    };
-    print_diagsums((int *)c3, 3);
-    print_diagsums((int *)c5, 5);
+	char *s = "hello, world. The world is great";
+
+    char *f = "world";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
     return (0);
 }
