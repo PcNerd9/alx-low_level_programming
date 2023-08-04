92eb82a68f54b9f950a23fdeda313c140a923a58
PcNerd <ajayihabeeb977@gmail.com>
2023-08-04 13:08:03 +0100

    wrote a c program that compute the sum of numbers passed to the program as a command line arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - convert a string to integer
+ * @s: a pointerto the string
+ *
+ * Return: the integer value of the string
+ */
+int _atoi(char *s)
+{
+	int i;
+	int num = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			num = num * 10 + (s[i] - '0');
+		else
+			return (num);
+	}
+	return (num);
+}
+/**
+ * main - multiplies two numbers passed from the command line
+ * @argc: the number of arguments passed
+ * @argv: a pointer to the array of arguments passed
+ *
+ * Return: 0 is successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		mul *= _atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: a pointer to the string
+ *
+ * Return: an integer of the string
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int num = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+		}
+		else
+			return (0);
+	}
+	return (num);
+}
+/**
+ * main - adds positive numbers
+ * @argc: the number of arguments passed
+ * @argv: a pointer to the array of arguments passed
+ *
+ * Return: the sum of the numbers
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int num;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		num = _atoi(argv[i]);
+		if (num == 0 && argv[i] != 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
