b5772edea1f6bf678b586bb0b69a766b064b7a17
PcNerd <ajayihabeeb977@gmail.com>
2023-08-07 12:10:31 +0100

    wrote a c function tthat returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,10 +3,12 @@
 
 
 /**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: the size of the array
+ *@c: the characterto initialize the array with
  *
- *
- *
- *
+ * Return: a pointer to the array
  */
 
 char *create_array(unsigned int size, char c)
@@ -18,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 	{
 		return (NULL);
-	}	
+	}
 	ptr = (char *)malloc(sizeof(char) * size);
 	if (ptr == NULL)
 	{
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * _strdup - creates a newly allocated spaces in memory, which contains a copy
+ * of the string given as a parameter
+ *@str: a pointer to the string to copy into the memory
+ *
+ *Return: a pointer to the newly created string
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int lenght;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		lenght++;
+	}
+	ptr = (char *)malloc(sizeof(char) * lenght);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < lenght; i++)
+	{
+		*(ptr + i) = *(str + i);
+	}
+	*(ptr + lenght) = '\0';
+	return (ptr);
+}
