8b97f827f47cb0d4c32da93d982f2aebfcb05244
PcNerd <ajayihabeeb977@gmail.com>
2023-08-14 19:42:34 +0100

    wrote a c function that creates a new dog using malloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,45 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - compute the lenght of a string
+ * @s: a pointer to the string
+ *
+ * Return: the lenghtof the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int lenght = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		lenght++;
+	}
+	return (lenght);
+}
+
+/**
+ * _strcpy - copy all the element in one location to another location
+ * @des: a pointer to the destination
+ * @src: a pointer tot he source location
+ *
+ * Return: a pointer to the destination location
+ */
+
+char *_strcpy(char *des, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		des[i]  = src[i];
+	}
+	des[i]   = '\0';
+	return (des);
+}
+
+/**
  * new_dog - it creates a new dog from the struct dog
  *@name: the argument to initialize the new dog elements with
  *@age: the argument to initialize the ne dog with
@@ -9,7 +48,6 @@
  *
  * Return: a pointe to the new dog created
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *create_dog;
@@ -17,8 +55,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	create_dog = (dog_t *)malloc(sizeof(dog_t));
 	if (create_dog == NULL)
 		return (NULL);
-	create_dog->name = name;
+	create_dog->name = (char *)malloc(_strlen(name) + 1);
+	_strcpy(create_dog->name, name);
+	if (create_dog->name == NULL)
+	{
+		free(create_dog);
+		return (NULL);
+	}
+
 	(*create_dog).age = age;
-	create_dog->owner = owner;
+
+	create_dog->owner = (char *)malloc(_strlen(name) + 1);
+	_strcpy(create_dog->owner, owner);
+	if (create_dog->owner == NULL)
+	{
+		free(create_dog->name);
+		free(create_dog);
+		return (NULL);
+
+
 	return (create_dog);
 }
--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -10,7 +10,7 @@ int main(void)
 {
     struct dog my_dog;
 
-    init_dog(&my_dog, "Poppy", 3.5, "");
+    init_dog(&my_dog, "Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof! and he is owned by %s\n", my_dog.name, my_dog.age, my_dog.owner);
     return (0);
 }
