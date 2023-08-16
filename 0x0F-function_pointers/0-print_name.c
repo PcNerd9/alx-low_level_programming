0ddd4c90b601a644aeaad00ee1467bb8061b92a1
PcNerd <ajayihabeeb977@gmail.com>
2023-08-16 21:35:03 +0100

    wrote a c function that searches for an integer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - the function call a function that print a name
+ * @name: a pointer to the name to print
+ * @f: a pointer to the function to be called
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This repo help me graps the concept of the use of function pointer and how to use the pointer to a function as a callaback function
