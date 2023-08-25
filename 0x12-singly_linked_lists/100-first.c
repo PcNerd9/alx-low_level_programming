4ed0a47430a4d45d257cb97a05db625b2c2919bf
PcNerd <ajayihabeeb977@gmail.com>
2023-08-25 11:55:23 +0100

    wrote a c function that  execute before the main funtcion is invoked

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_before_main - execute before main function
+ *
+ * Return: nothing
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
