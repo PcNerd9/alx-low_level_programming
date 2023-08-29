1fdbd78aa861627f84f7edb046e2c4f1328da2ca
PcNerd <ajayihabeeb977@gmail.com>
2023-08-29 22:38:41 +0100

    wrote a c function that reverse the order of a linked list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - it reverse the order of the list
+ * @head: a pointer to the address of the head node
+ *
+ * Return: a pointer to the head node of the reverse list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *temp, *previous_node, *next_node;
+
+	previous_node = *head;
+	next_node = previous_node->next;
+	previous_node->next = NULL;
+
+	while (next_node != NULL)
+	{
+		temp = next_node->next;
+		next_node->next = previous_node;
+		previous_node = next_node;
+		next_node = temp;
+	}
+	*head = previous_node;
+	return (*head);
+}
