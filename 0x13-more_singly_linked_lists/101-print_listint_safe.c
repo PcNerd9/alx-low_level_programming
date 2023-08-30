f090459eec1253105e961d0143cabfb89085b8d8
PcNerd <ajayihabeeb977@gmail.com>
2023-08-30 20:31:20 +0100

    wrote a c function that checks if a linked list has a loop using floyd's algorithm

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,25 +1,81 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
+
+
+size_t print_nodes(const listint_t **head, const listint_t *end);
+const listint_t *check_if_list_has_loop(const listint_t *head);
 
 /**
+ * print_listint_safe - safely prints all the elements in a list
+ * containt loop
+ *@head: a pointer to the head node
  *
- *
- *
- *
- *
- *
+ *Return: the number of nodes in the list
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *temp;
+	const listint_t *begin_loop, *temp;
 	size_t lenght = 0;
 
-	temp = head;
-	do {
-		printf("[%p] %d\n", (void *)&(temp->n), temp->n);
-		temp = temp->next;
-		lenght++;
-	} while (temp != head);
+	if (head != NULL)
+	{
+		begin_loop = check_if_list_has_loop(head);
+		temp = head;
+		do {
+			printf("[%p] %d\n", (void *)&temp->n, temp->n);
+			temp = temp->next;
+			lenght++;
+		} while (temp != begin_loop);
+
+		if (begin_loop != NULL)
+		{
+		do {
+			printf("[%p] %d\n", (void *)&temp->n, temp->n);
+			temp = temp->next;
+			lenght++;
+		} while (temp != begin_loop);
+		}
+	}
 	return (lenght);
+}
+
+/**
+ * check_if_list_has_loop - checks if the list as loop
+ * @head: a pointer to the head node
+ *
+ * Return: the begining of the loop otherwise it return NULL
+ */
+
+const listint_t *check_if_list_has_loop(const listint_t *head)
+{
+	const listint_t *tortise, *hare;
+	int contain_loop  = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	tortise = head;
+	hare =  head;
+	while (hare != NULL && hare->next != NULL)
+	{
+		hare = (hare->next)->next;
+		tortise = tortise->next;
+		if (hare == tortise)
+		{
+			contain_loop = 1;
+			break;
+	}
+	}
+	if (contain_loop)
+	{
+		tortise  = head;
+		do {
+			tortise = tortise->next;
+			hare = hare->next;
+		} while (tortise != hare);
+		return (hare);
+	}
+	return (NULL);
+
 }
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - evaluate the begining of a loop in a linked list
+ * @head: a pointer to the head of the node
+ *
+ * Return: the begining loop node otherwise return NULL
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortise, *hare;
+
+	tortise = hare = head;
+
+	while (hare != NULL && hare->next != NULL)
+	{
+		hare = (hare->next)->next;
+		tortise = tortise->next;
+		if (hare == tortise)
+			break;
+	}
+	if (hare != NULL)
+	{
+		tortise = head;
+		do {
+			tortise = tortise->next;
+			hare = hare->next;
+		} while (tortise != hare);
+	}
+	return (hare);
+}
