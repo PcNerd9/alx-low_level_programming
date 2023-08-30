2ddac0ae9e3c500c3ed7a5be977dfbe2ff0bccdf
PcNerd <ajayihabeeb977@gmail.com>
2023-08-30 01:10:34 +0100

    debug the 9-insert_node file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -44,7 +44,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	temp = *head;
-	for (i = 0; i < idx; i++)
+	for (i = 0; i <= idx; i++)
 	{
 		previous_node = temp;
 		temp = temp->next;
