1271d59146ffa8f93b5bfcb405674accdff951ee
PcNerd <ajayihabeeb977@gmail.com>
2023-08-31 21:52:53 +0100

    debugging the 2-get_bit.c file

--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -1,20 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h> 
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    unsigned long int n;
+int main() {
+    unsigned long int testCases[] = { 0, 255, 123456789, 18446744073709551615UL };
+    unsigned long int i, j;
+    unsigned int indices[] = { 0, 3, 7, 31, 63, 65 };
 
-    n = get_bit(1024, 10);
-    printf("%ld\n", n);
-    n = get_bit(184467440737095516, 50);
-    printf("%ld\n", n);
-    n = get_bit(1024, 0);
-    printf("%ld\n", n);
-    return (0);
+    for (i = 0; i < sizeof(testCases) / sizeof(testCases[0]); ++i) {
+        for (j = 0; j < sizeof(indices) / sizeof(indices[0]); ++j) {
+            int bitValue = get_bit(testCases[i], indices[j]);
+
+            if (bitValue != -1) {
+                printf("Bit at index %u of %lu is: %d\n", indices[j], testCases[i], bitValue);
+            } else {
+                printf("Error: Invalid bit index\n");
+            }
+        }
+    }
+
+    return 0;
 }
