e5fca2eb2f2c0086c5464e40bd6362ff91fb4ed0
PcNerd <ajayihabeeb977@gmail.com>
2023-08-31 21:55:32 +0100

    debug the 3-set_bit.c file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,12 +12,11 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int compare;
 
-	compare = 1 << index;
-
 	if (n == NULL)
 		return (-1);
-	if (compare > *n)
+	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
+	compare = 1 << index;
 	*n = *n | compare;
 	return (1);
 }
