eb51fe393beb2a2e5e099f3548d177614db8c3a4
PcNerd <ajayihabeeb977@gmail.com>
2023-09-28 23:58:34 +0100

    wrote a c function that print a number in it binary format

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "bit.h"
+
+/**
+ * print_binary - print a number in binary
+ * @n: the number to print
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	putchar ('0' + (n & 1));
+}
--- 0x14-bit_manipulation/bit.h
+++ 0x14-bit_manipulation/bit.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef BIT_H
+#define BIT_H
 
 #include <stddef.h>
 int _putchar(char c);
