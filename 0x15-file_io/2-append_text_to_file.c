b9a26713046a511377621e9e25355ef76e6985e4
PcNerd <ajayihabeeb977@gmail.com>
2023-10-02 20:27:37 +0100

    resolve bety issue

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/**
+ * _strlen - computethel lenght of string
+ * @string: the string to compute
+ *
+ * Return: the number computer
+ */
 int _strlen(char *string)
 {
 	int lenght = 0;
@@ -12,6 +18,14 @@ int _strlen(char *string)
 	}
 	return (lenght);
 }
+
+/**
+ * append_text_to_file - append to a file
+ * @filename: the name of the file
+ * @text_content: the text to append
+ *
+ * Return: 0 on success
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	pid_t fd, is_exit;
