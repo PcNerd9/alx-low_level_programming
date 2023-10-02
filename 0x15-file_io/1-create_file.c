b59d97b82256cd9286317a2065076a5713fdb190
PcNerd <ajayihabeeb977@gmail.com>
2023-10-02 19:54:22 +0100

    resolve the isse

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -45,7 +45,7 @@ int create_file(const char *filename, char *text_content)
 		bytewrite = write(fd, text_content, _strlen(text_content));
 		if (bytewrite <= 0)
 		{
-			 close(fd);
+			close(fd);
 			return (-1);
 		}
 	}
