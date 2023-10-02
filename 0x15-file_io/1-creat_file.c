56362fe699738df9affa7ad3322d3c9e725dad81
PcNerd <ajayihabeeb977@gmail.com>
2023-10-02 10:54:40 +0100

    wrote a c function that creates or write to an existing file

---
+++ 0x15-file_io/1-creat_file.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file - create a new file or write to an existing file
+ * @filename: the name of the file
+ * @text_content: the content to write to the file
+ *
+ * Return: 1 on success otherwise return -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	pid_t fd;
+	ssize_t bytewrite;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd <= 0)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		bytewrite = write(fd, text_content, strlen(text_content));
+		if (bytewrite <= 0)
+		{
+			return (-1);
+		}
+	}
+	return (1);
+}
