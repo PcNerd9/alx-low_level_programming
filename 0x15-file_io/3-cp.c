f6b886d0ecbd404136f06ab9a6d7ab3fdee284c5
PcNerd <ajayihabeeb977@gmail.com>
2023-10-03 03:16:10 +0100

    debugging the 3-cp.c file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 			bytewrite = write(dest_fd, buffer, byteread);
 			if (bytewrite <= 0)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				dprintf(STDERR_FILENO, "Error: Can't rite to %s\n", argv[2]);
 				exit(99);
 			}
 		}
