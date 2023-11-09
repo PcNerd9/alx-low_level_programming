3aa3a523350a59728ab140dd04ee41f517184eaf
PcNerd <ajayihabeeb977@gmail.com>
2023-11-09 22:10:23 +0100

    resolve compilation issue in 2-add_dnodeint.c file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		*head = new_node;
 		return (new_node);
 	}
-	head->prev = new_node;
+	(*head)->prev = new_node;
 	new_node->next = *head;
 	new_node->prev = NULL;
 	*head = new_node;
