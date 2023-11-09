66ab0876b1f217c698b7de7949a23e700451d002
PcNerd <ajayihabeeb977@gmail.com>
2023-11-09 21:43:27 +0100

    resolvethe bugin 2-add_dnodeint.c file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,6 +18,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 	new_node->next = *head;
 	new_node->prev = NULL;
 	*head = new_node;
