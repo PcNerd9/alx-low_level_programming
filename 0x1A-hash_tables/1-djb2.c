e4ad47df42ba762c89c8f9207dbf829dd456027e
PcNerd <ajayihabeeb977@gmail.com>
2023-12-02 05:42:00 +0100

    implement the hashtable

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,13 +2,13 @@
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
-	int c;
+	unsigned long int hash_value;
+	int car;
 
-	hash = 5381;
+	hash_value = 5381;
 	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + c;
+		hash_value = (hash * 33) + car;
 	}
-	return (hash);
+	return (hash_value);
 }
