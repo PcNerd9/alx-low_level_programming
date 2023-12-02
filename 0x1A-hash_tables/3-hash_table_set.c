c6404d9bd30262ef48ed7a602530ce36590d5988
PcNerd <ajayihabeeb977@gmail.com>
2023-12-02 12:19:52 +0100

    debugging

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,6 +38,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *current_node;
 
+	if (ht == NULL)
+		return (0);
 	if (key == NULL || *key == '\0' || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -11,7 +11,6 @@
 int main(void)
 {
     hash_table_t *ht;
-    char *key;
     char *value;
 
     ht = hash_table_create(1024);
@@ -21,22 +20,33 @@ int main(void)
     hash_table_set(ht, "N", "queens");
     hash_table_set(ht, "Asterix", "Obelix");
     hash_table_set(ht, "Betty", "Cool");
-    hash_table_set(ht, "98", "Battery Streetz");
-    key = strdup("Tim");
-    value = strdup("Britton");
-    hash_table_set(ht, key, value);
-    key[0] = '\0';
-    value[0] = '\0';
-    free(key);
-    free(value);
-    hash_table_set(ht, "", "Battery Street");
-    hash_table_set(ht, "98", "");
-	hash_table_set(ht, "98", "Battery Street"); 
-    hash_table_set(ht, "hetairas", "Bob");
-    hash_table_set(ht, "hetairas", "Bob Z");
-    hash_table_set(ht, "mentioner", "Bob");
-    hash_table_set(ht, "hetairas", "Bob Z Chu");
-    hash_table_print(ht);
-    hash_table_delete(ht);
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_set(ht, "c", "isfun");
+    hash_table_set(ht, "hetairas", "string");
+    hash_table_set(ht, "mentioner", "collide");
+	hash_table_set(NULL, "another", "another");
+    value = hash_table_get(ht, "python");
+    printf("%s:%s\n", "python", value);
+    value = hash_table_get(ht, "Bob");
+    printf("%s:%s\n", "Bob", value);
+    value = hash_table_get(ht, "N");
+    printf("%s:%s\n", "N", value);
+    value = hash_table_get(ht, "Asterix");
+    printf("%s:%s\n", "Asterix", value);
+    value = hash_table_get(ht, "Betty");
+    printf("%s:%s\n", "Betty", value);
+    value = hash_table_get(ht, "98");
+    printf("%s:%s\n", "98", value);
+    value = hash_table_get(ht, "c");
+    printf("%s:%s\n", "c", value);
+    value = hash_table_get(ht, "javascript");
+    printf("%s:%s\n", "javascript", value);
+    value = hash_table_get(ht, "hetairas");
+    printf("%s:%s\n", "hetairas", value);
+    value = hash_table_get(ht, "mentioner");
+   printf("%s:%s\n", "mentioner", value);
+   value = hash_table_get(NULL, "another" );
+	printf("%s:%s\n", "another", value);
+
     return (EXIT_SUCCESS);
 }
